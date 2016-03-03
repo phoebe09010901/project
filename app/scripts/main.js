510e2534921526d361df8acb96cb25112ef6f4aa
Phoebe <phy4175@gmail.com>
2016-03-03 21:23:55 +0800

    initial

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+console.log('\'Allo \'Allo!');
+
+jQuery(document).ready(function($) {
+	var picker = new Pikaday({ field: document.getElementById('datepicker') });
+    $('.jcarousel').jcarousel({
+        // Configuration goes here
+    });
+});
